bffaa066f989737536edda0dbd2423548d41525a
Matheus22Garrido <theusgarrido22@gmail.com>
2026-04-28 01:21:17 +0000

    ex1 ok

--- Ex 1.asm
+++ Ex 1.asm
@@ -1,10 +1,39 @@
+#  $v0 serve pra escrita e leitura
+#  4 serve pra escrita de caracter
+#  5 serve pra leitura de inteiros
+#  1 serve pra escrita de inteiros
+
 .data
+    msg1: .asciiz "\nDigite o primeiro lado do terreno: "
+    msg2: .asciiz "\nDigite o segundo lado do terreno: "
+    msg3: .asciiz "\nA área do terreno é: "
+
 .text
-    msg1: 
-    msg2:
-    msg3:
-    msg4:
-    msg5:
+main:
+    # lê o valor do primeiro lado
+    li $v0, 4
+    la $a0, msg1 
+    syscall
 
+    li $v0, 5
+    syscall
 
-    teste do read.me
+    add $t0, $v0, 0
+
+    li $v0, 4
+    la $a0, msg2
+    syscall 
+
+    li $v0, 5
+    syscall
+
+    add $t1, $v0, 0
+
+    mul $t2, $t1, $t0
+
+    li $v0, 4
+    la $a0, msg3
+    syscall
+    li $v0, 1
+    add $a0, $t2, 0
+    syscall
