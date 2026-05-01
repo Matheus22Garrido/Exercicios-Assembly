be0968297736966fcbe43c6793af90eb9c1c8825
Matheus22Garrido <theusgarrido22@gmail.com>
2026-05-01 18:28:04 +0000

    ex 7 e 8 ok

--- Lista1/Ex7.asm
+++ Lista1/Ex7.asm
@@ -1,9 +1,38 @@
 .data
-    msg1: .asciiz "\n"
-    msg2: .asciiz "\n"
-    msg3: .asciiz "\n"
-    msg4: .asciiz "\n"
-    msg5: .asciiz "\n"
-    msg6: .asciiz "\n"
+    msg1: .asciiz "\nDigite seu número: "
+    msg2: .asciiz "\nO resultado da soma é: "
 .text
 main:
+    # recebe valor
+    li $v0, 4
+    la $a0, msg1
+    syscall
+    li $v0, 5
+    syscall 
+    add $t9, $v0, 0
+
+    # verifica se é par ou ímpar
+    rem $s0, $t9, 2
+    beq $s0, 0, if
+    bne $s0, 0, else
+
+if: 
+    # caso número par
+    add $s1, $t9, 5
+
+    j resultado
+
+else:
+    # caso número ímpar
+    add $s1, $t9, 8
+
+    j resultado
+
+resultado:
+    # mostra resultado
+    li $v0, 4
+    la $a0, msg2
+    syscall
+    li $v0, 1
+    add $a0, $s1, 0
+    syscall
--- Lista1/Ex8.asm
+++ Lista1/Ex8.asm
@@ -1,9 +1,34 @@
 .data
-    msg1: .asciiz "\n"
-    msg2: .asciiz "\n"
-    msg3: .asciiz "\n"
-    msg4: .asciiz "\n"
-    msg5: .asciiz "\n"
-    msg6: .asciiz "\n"
+    msg1: .asciiz "\nDigite seu número: "
+    msg2: .asciiz "\nO resultado é: "
 .text
 main:
+    # recebe valor
+    li $v0, 4
+    la $a0, msg1
+    syscall
+    li $v0, 5
+    syscall 
+    add $t9, $v0, 0
+
+    # verifica se é positivo ou negativo
+    blt $t9, 0, negativo
+    bgt $t9, 0, positivo
+
+positivo:
+    # caso número positivo
+    mul $s0, $t9, 2
+    j resultado
+
+negativo:
+    # caso número negativo
+    mul $s0, $t9, 3
+    j resultado
+
+resultado:
+    li $v0, 4
+    la $a0, msg2
+    syscall
+    li $v0, 1
+    add $a0, $s0, 0
+    syscall
